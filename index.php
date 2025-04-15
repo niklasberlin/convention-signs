86cc3aada9700fe5add513bc6b52fe3aa19d16e8
Niklas Aumüller <git@niklas-aumueller.de>
2025-04-16 01:09:48 +0200

    removed empty stylsheet

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
         <title>Aichtal 2025 PDF Generator</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body class="bg-dark text-light">
