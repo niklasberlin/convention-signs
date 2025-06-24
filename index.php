b189da798813dc41fa6a413eaca9e6799722bcde
Niklas Aumüller <git@niklas-aumueller.de>
2025-06-24 17:23:52 +0200

    fixed title on index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Aichtal 2025 PDF Generator</title>
+        <title>EJC 2025 PDF Generator</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -17,7 +17,7 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         <div class="container d-flex h-100 p-3 mx-auto flex-column">
-            <h2>Aichtal 2025 PDF generator</h2>
+            <h2>EJC 2025 PDF generator</h2>
             <div class="container mt-3 border border-secondary p-3">
                 <h3>Website QR-Code Signs</h3>
                 <form action="website.php" method="get">
